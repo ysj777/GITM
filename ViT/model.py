68e7e0b94e371fdcdd35e8dedfcdc9f08fdb606e
yuming <69292566+1072938@users.noreply.github.com>
2023-03-26 20:25:11 +0800

    加入 decoder

--- ViT/model.py
+++ ViT/model.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 import torch
 from transformers import ViTConfig, ViTModel
 
@@ -12,17 +13,19 @@ class ViT(nn.Module):
         self.batch_size = batch_size
         self.patch_size = patch_size
         self.input_history = input_history
-        self.hidden_dim = 768
-        # self.fc_input = nn.Linear(self.in_dim, self.hidden_dim, device=device)
-        # self.fc_output = nn.Linear(self.hidden_dim, self.out_dim, device=device)
-        self.configuration = ViTConfig(image_size = 72, num_channels = self.input_history, patch_size = self.patch_size)
+        self.hidden_dim = 144
+        self.configuration = ViTConfig(image_size = 72, hidden_size= self.hidden_dim, num_channels = self.input_history, patch_size = self.patch_size)
         self.model = ViTModel(self.configuration)
-        self.decoder = nn.Linear(self.hidden_dim, out_dim, device=device)
-    
-    def forward(self, tec):
+        self.decoder_layer = nn.TransformerDecoderLayer(d_model = self.hidden_dim, nhead=8)
+        self.transformer_decoder = nn.TransformerDecoder(self.decoder_layer, num_layers=num_layers)
+        self.fc = nn.Linear(self.hidden_dim, out_dim, device=device)
+
+    def forward(self, tec, tec_target):
         output = self.model(tec)
+        # print(np.array(tec_target.clone().detach().cpu()).shape[:])
         # print(output['last_hidden_state'].shape[:])
-        output = self.decoder(output['last_hidden_state'][:, 1:])
+        output = self.transformer_decoder(tec_target, output['last_hidden_state'][:, 1:])
+        output = self.fc(output)
         # print(output.shape[:])
         return output
         
--- ViT/train_model.py
+++ ViT/train_model.py
@@ -35,7 +35,7 @@ def train_model(dataloader, valid_dataloader, in_dim, out_dim, batch_size, patch
             for param in model.parameters(): param.grad = None
             b_input, b_target = tuple(b.to(device) for b in batch[:2])
             # b_information = batch[3].to(device)
-            output = model(b_input)
+            output = model(b_input, b_target)
             
             loss = torch.sqrt(mse(output, b_target))
             loss.backward()
